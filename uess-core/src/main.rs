6fd304679864a940ac5b91a8f61c8b2067e32a74
brysonak <brysonak@protonmail.com>
2026-06-17 12:44:50 -0400

    Add the cat replies, I hate all of you

--- uess-core/src/main.rs
+++ uess-core/src/main.rs
@@ -10,6 +10,17 @@ struct Handler {
     honeypot_channel: Option<ChannelId>,
 }
 
+// It's a server surrounding something arch-linux based, if you didn't expect there to be cat stuff in here then that's your fault
+fn cat_reply(content: &str) -> Option<&'static str> {
+    content
+        .split_whitespace()
+        .find_map(|word| match word.to_lowercase().as_str() {
+            "nya" => Some("Nya!"),
+            "meow" => Some("mrrrow"),
+            _ => None,
+        })
+}
+
 #[async_trait]
 impl EventHandler for Handler {
     async fn ready(&self, ctx: Context, ready: Ready) {
@@ -33,6 +44,12 @@ impl EventHandler for Handler {
 
     async fn message(&self, ctx: Context, msg: Message) {
         honeypot::handle(&ctx, &msg, self.honeypot_channel).await;
+
+        if !msg.author.bot {
+            if let Some(reply) = cat_reply(&msg.content) {
+                let _ = msg.channel_id.say(&ctx.http, reply).await;
+            }
+        }
     }
 
     async fn interaction_create(&self, ctx: Context, interaction: Interaction) {
