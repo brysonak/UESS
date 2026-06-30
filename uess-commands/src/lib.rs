e053a366d2b81a5b7fcd05c59bc1ef78e570b814
brysonak <brysonak@protonmail.com>
2026-06-30 15:51:00 -0400

    I forgot to register the command

--- uess-commands/src/lib.rs
+++ uess-commands/src/lib.rs
@@ -6,12 +6,13 @@ pub mod source_code;
 use serenity::all::{CommandInteraction, Context, CreateCommand};
 
 pub fn register_commands() -> Vec<CreateCommand> {
-    vec![aur_check::register(), source_code::register()]
+    vec![aur_check::register(), cleanse::register(), source_code::register()]
 }
 
 pub async fn dispatch(ctx: &Context, command: &CommandInteraction) {
     match command.data.name.as_str() {
         "aur-check" => aur_check::run(ctx, command).await,
+        "cleanse" => cleanse::run(ctx, command).await,
         "source-code" => source_code::run(ctx, command).await,
         _ => {}
     }
